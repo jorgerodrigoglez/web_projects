87148983b57d2c890501183a1f83d420318793af
Jorge Rodrigo Gonzalez <jorgerodrigoglez@gmail.com>
2017-04-25 21:20:03 +0200

    creando los archivos dist

---
+++ dist/index.php
@@ -0,0 +1,1 @@
+<!DOCTYPE html><html lang="es"><head><meta charset="UTF-8"><title>Document</title><link href="https://fonts.googleapis.com/css?family=Barrio" rel="stylesheet"><link rel="stylesheet" href="./css/style.css"></head><body> <?php echo '<h1>Hola Mundo :) ' . (8 + 6) . '</h1>'; ?> <script src="./js/script.js"></script></body></html>
